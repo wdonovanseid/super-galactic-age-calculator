5aab29f78e5aec55b8cef1c268ca878a4e5fb26c
William Donovan-Seid <wdstwo@live.com>
2020-09-04 14:42:55 -0700

    try to get display function shorter by looping...again

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import {GalacticAge} from './js/age-calculator';
 function displayGalInfo(userGalAge) {
   $("#output").show();
   //! Arrays for both Attempts at looping
-  // const planetArrayAge=["mercAge","venusAge","marsAge","jupAge"];
+  const planetArrayAge=["mercAge","venusAge","marsAge","jupAge"];
+  planetArrayAge.forEach(function(planetAge) {
+    $("#"+planetAge).text(userGalAge.planetAge);
+  });
   // const planetArrayAgeAvg=["mercAgeAvg","venusAgeAvg","marsAgeAvg","jupAgeAvg"];
   //! Attempt 2
   //   for (let i=0; i<4; i++) {
@@ -28,34 +31,34 @@ function displayGalInfo(userGalAge) {
   //     $("#"+planetAge+"Avg").text(`You have ${userGalAge.planetAge+"Avg"} years left to live here.`);
   //   }
   // });
-  $("#mercAge").text(userGalAge.mercAge);
-  if (userGalAge.mercAgeAvg < 0) {
-    userGalAge.mercAgeAvg *= -1;
-    $("#mercAgeAvg").text(`You have lived ${userGalAge.mercAgeAvg} years past your life expectancy here.`);
-  } else {
-    $("#mercAgeAvg").text(`You have ${userGalAge.mercAgeAvg} years left to live.`);
-  }
-  $("#venusAge").text(userGalAge.venusAge);
-  if (userGalAge.venusAgeAvg < 0) {
-    userGalAge.venusAgeAvg *= -1;
-    $("#venusAgeAvg").text(`You have lived ${userGalAge.venusAgeAvg} years past your life expectancy here.`);
-  } else {
-    $("#venusAgeAvg").text(`You have ${userGalAge.venusAgeAvg} years left to live.`);
-  }
-  $("#marsAge").text(userGalAge.marsAge);
-  if (userGalAge.marsAgeAvg < 0) {
-    userGalAge.marsAgeAvg *= -1;
-    $("#marsAgeAvg").text(`You have lived ${userGalAge.marsAgeAvg} years past your life expectancy here.`);
-  } else {
-    $("#marsAgeAvg").text(`You have ${userGalAge.marsAgeAvg} years left to live.`);
-  }
-  $("#jupAge").text(userGalAge.jupAge);
-  if (userGalAge.jupAgeAvg < 0) {
-    userGalAge.jupAgeAvg *= -1;
-    $("#jupAgeAvg").text(`You have lived ${userGalAge.jupAgeAvg} years past your life expectancy here.`);
-  } else {
-    $("#jupAgeAvg").text(`You have ${userGalAge.jupAgeAvg} years left to live.`);
-  }
+//   $("#mercAge").text(userGalAge.mercAge);
+//   if (userGalAge.mercAgeAvg < 0) {
+//     userGalAge.mercAgeAvg *= -1;
+//     $("#mercAgeAvg").text(`You have lived ${userGalAge.mercAgeAvg} years past your life expectancy here.`);
+//   } else {
+//     $("#mercAgeAvg").text(`You have ${userGalAge.mercAgeAvg} years left to live.`);
+//   }
+//   $("#venusAge").text(userGalAge.venusAge);
+//   if (userGalAge.venusAgeAvg < 0) {
+//     userGalAge.venusAgeAvg *= -1;
+//     $("#venusAgeAvg").text(`You have lived ${userGalAge.venusAgeAvg} years past your life expectancy here.`);
+//   } else {
+//     $("#venusAgeAvg").text(`You have ${userGalAge.venusAgeAvg} years left to live.`);
+//   }
+//   $("#marsAge").text(userGalAge.marsAge);
+//   if (userGalAge.marsAgeAvg < 0) {
+//     userGalAge.marsAgeAvg *= -1;
+//     $("#marsAgeAvg").text(`You have lived ${userGalAge.marsAgeAvg} years past your life expectancy here.`);
+//   } else {
+//     $("#marsAgeAvg").text(`You have ${userGalAge.marsAgeAvg} years left to live.`);
+//   }
+//   $("#jupAge").text(userGalAge.jupAge);
+//   if (userGalAge.jupAgeAvg < 0) {
+//     userGalAge.jupAgeAvg *= -1;
+//     $("#jupAgeAvg").text(`You have lived ${userGalAge.jupAgeAvg} years past your life expectancy here.`);
+//   } else {
+//     $("#jupAgeAvg").text(`You have ${userGalAge.jupAgeAvg} years left to live.`);
+//   }
 }
 
 function attachListeners() {
