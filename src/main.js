b57ed0f8193084c7c0a279f67c178c3c8d86b93f
William Donovan-Seid <wdstwo@live.com>
2020-09-04 11:07:36 -0700

    try to loop through all ids for displayGalInfo function

--- src/main.js
+++ src/main.js
@@ -4,6 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import {GalacticAge} from './js/age-calculator';
 
+function displayGalInfo(userGalAge) {
+  $("#output").show();
+  const planetArray=["mercAge","venusAge","marsAge","jupAge"];
+  planetArray.forEach(function(planetAge) {
+    $("#"+planetAge).text(userGalAge.planetAge);
+    if (userGalAge.planetAge+"Avg" < 0) {
+      $("#"+planetAge+"Avg").text("You have lived "+(-userGalAge.planetAge+"Avg")+" years past your life expectancy here.");
+    } else {
+      $("#"+planetAge+"Avg").text("You have "+userGalAge+"."+planetAge+"Avg"+" years left to live.");
+    }
+  });
+  $("#mercAge").text(userGalAge.mercAge);
+  if (userGalAge.mercAgeAvg < 0) {
+    $("#merc-age-avg").text("You have lived "+(-userGalAge.mercAgeAvg)+" years past your life expectancy here.");
+  } else {
+    $("#merc-age-avg").text("You have "+userGalAge.mercAgeAvg+" years left to live.");
+  }
+}
+
 $(document).ready(function() {
   $("form#user-info").submit(function(event) {
     event.preventDefault();
@@ -14,12 +33,6 @@ $(document).ready(function() {
     let userGalAge = new GalacticAge(userAge,userDrink,userGender,userExercise);
     userGalAge.calcAvg();
     userGalAge.lifeLeft();
-    $("#output").show();
-    $("#merc-age").text(userGalAge.mercAge);
-    if (userGalAge.mercAgeAvg < 0) {
-      $("#merc-age-avg").text("You have lived "+(-userGalAge.mercAgeAvg)+" years past your life expectancy here.");
-    } else {
-      $("#merc-age-avg").text("You have "+userGalAge.mercAgeAvg+" years left to live.");
-    }
+    displayGalInfo(userGalAge);
   });
 });
