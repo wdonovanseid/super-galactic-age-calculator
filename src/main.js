7be1984cdf0fe1cb00af1657c1c657202c94540f
William Donovan-Seid <wdstwo@live.com>
2020-09-04 10:40:08 -0700

    add in basic inputs in index.html

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import './css/styles.css';
 import {GalacticAge} from './js/age-calculator';
 
 $(document).ready(function() {
-  $("form#userInfo").submit(function(event) {
+  $("form#user-info").submit(function(event) {
     event.preventDefault();
-    const userAge = parseInt($("input#user-age").val());
-    const userDrink = $("input#user-drink").val();
-    const userGender = $("input#user-gender").val();
-    const userExercise = $("input#user-exercise").val();
+    const userAge = $("input#user-age").val();
+    const userDrink = $("input[name='user-gender']:checked").val();
+    const userGender = $("input[name='user-drink']:checked").val();
+    const userExercise = $("input[name='user-exercise']:checked").val();
     let userGalAge = new GalacticAge(userAge,userDrink,userGender,userExercise);
     userGalAge.calcAvg();
     userGalAge.lifeLeft();
     $("#output").show();
     $("#merc-age").text(userGalAge.mercAge);
     if (userGalAge.mercAgeAvg < 0) {
-      $("#merc-age-avg").text("You have lived "+(-userGalAge.mercAgeAvg)+" years past your life expectancy here.")
+      $("#merc-age-avg").text("You have lived "+(-userGalAge.mercAgeAvg)+" years past your life expectancy here.");
     } else {
-      $("#merc-age-avg").text("You have "+userGalAge.mercAgeAvg+" years left to live.")
+      $("#merc-age-avg").text("You have "+userGalAge.mercAgeAvg+" years left to live.");
     }
   });
 });
