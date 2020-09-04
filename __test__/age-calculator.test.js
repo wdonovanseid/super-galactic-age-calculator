a5b9d5b8af40b736895b36b3ea47df028c4f841f
William Donovan-Seid <wdstwo@live.com>
2020-09-04 08:49:09 -0700

    add in 2nd, 3rd, and 4th TDD tests

--- __test__/age-calculator.test.js
+++ __test__/age-calculator.test.js
@@ -8,6 +8,18 @@ describe('GalacticAge', ()=> {
   });
 
   test('should return user age converted to mercury years', ()=> {
-    expect(galAge.mercAge).toEqual(10.8);
+    expect(galAge.mercAge).toEqual(11);
+  });
+
+  test('should return user age converted to venus years', ()=> {
+    expect(galAge.venusAge).toEqual(28);
+  });
+
+  test('should return user age converted to mars years', ()=> {
+    expect(galAge.marsAge).toEqual(85);
+  });
+
+  test('should return user age converted to jupiter years', ()=> {
+    expect(galAge.jupAge).toEqual(534);
   });
 });
