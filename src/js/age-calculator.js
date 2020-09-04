2f0ff80c49ab4eca96e3e9cab26b902ff1d70910
William Donovan-Seid <wdstwo@live.com>
2020-09-04 08:53:31 -0700

    add in code to fulfill 2nd, 3rd, and 4th TDD tests

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -2,7 +2,8 @@ export class GalacticAge {
   constructor(userAge) {
     this.earthAge = userAge;
     this.mercAge = Math.round(userAge * 0.24);
-    this.venusAge;
-    this.jupAge;
+    this.venusAge = Math.round(userAge * 0.62);
+    this.marsAge = Math.round(userAge * 1.88);
+    this.jupAge = Math.round(userAge * 11.86);
   }
 }
